fc17d219cc2ebc75945fbdc2e93b11321193b2bc
BreHideki <brenohideki@hotmail.com>
2024-04-17 23:22:40 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+//Desafio 01
+
+alert('Hello World');
+// Desafio 02
+let number1 = 1;
+let number2 = 2;
+console.log(number1 + number2);
+//Desafio 03
+let thing = true;
+let number = 678;
+
+const isNumber = typeof thing === typeof number ?  'É um número' : 'Não é um número';
+
+console.log(isNumber);
+//Desafio 04
+let string = "Olá Mundo";
+
+const isString = typeof thing === typeof string ?  'É uma string' : 'Não é uma string';
+
+console.log(isString);
+// Desafio 05
+
+let boolean = true;
+
+const isBoolean = typeof thing === typeof boolean ?  'É um boolean' : 'Não é um boolean';
+
+console.log(isBoolean);
+
+//Desafio 06
+const sub = number1 - number2
+console.log(sub)
+
+//Desafio 07
+const mult = number1 * number2
+console.log(mult)
+
+//Desafio 08
+const div = number1 / number2
+console.log(div)
+
+//Desafio 09
+let evenOrOdd = number % 2 == 0;
+
+const isEven = evenOrOdd === true ? ` O número ${number} é par` : ` O número ${number} não é par`
+console.log(isEven)
+
+//Desafio 10
+
+const isOdd = evenOrOdd === false ? ` O número ${number} é ímpar` : ` O número ${number} não é ímpar`
+console.log(isOdd)
+
